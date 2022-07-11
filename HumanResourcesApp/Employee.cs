cfd28f108a9bc18bb57ffc6ad1bd1a4a37042144
RokerDev <rokerdev91@gmail.com>
2022-07-11 23:14:46 +0200

    set employee properties

--- HumanResourcesApp/Employee.cs
+++ HumanResourcesApp/Employee.cs
@@ -8,5 +8,18 @@ namespace HumanResourcesApp
 {
     class Employee
     {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime HiringDate { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Comments { get; set; }
+
+        public int Salary { get; set; }
     }
 }
