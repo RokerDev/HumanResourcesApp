2d928ec50646a62f1d8cbc7b0514ff8244230c64
RokerDev <rokerdev91@gmail.com>
2022-07-11 22:33:23 +0200

    set desing of adding and updating window

---
+++ HumanResourcesApp/AddUpdateWindow.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace HumanResourcesApp
+{
+    partial class AddUpdateEmployee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbFirstName = new System.Windows.Forms.Label();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.lbLastName = new System.Windows.Forms.Label();
+            this.tbHiring = new System.Windows.Forms.TextBox();
+            this.lbHiringDate = new System.Windows.Forms.Label();
+            this.tbRelease = new System.Windows.Forms.TextBox();
+            this.lbEmploymentDate = new System.Windows.Forms.Label();
+            this.tbComments = new System.Windows.Forms.TextBox();
+            this.lbComments = new System.Windows.Forms.Label();
+            this.tbSalary = new System.Windows.Forms.TextBox();
+            this.lbSalary = new System.Windows.Forms.Label();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.btnDismiss = new System.Windows.Forms.Button();
+            this.tbEmployeeId = new System.Windows.Forms.TextBox();
+            this.lbId = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbFirstName
+            // 
+            this.lbFirstName.AutoSize = true;
+            this.lbFirstName.Location = new System.Drawing.Point(12, 41);
+            this.lbFirstName.Name = "lbFirstName";
+            this.lbFirstName.Size = new System.Drawing.Size(57, 13);
+            this.lbFirstName.TabIndex = 0;
+            this.lbFirstName.Text = "First Name";
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Location = new System.Drawing.Point(101, 38);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(126, 20);
+            this.tbFirstName.TabIndex = 1;
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(101, 64);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(126, 20);
+            this.tbLastName.TabIndex = 3;
+            // 
+            // lbLastName
+            // 
+            this.lbLastName.AutoSize = true;
+            this.lbLastName.Location = new System.Drawing.Point(11, 67);
+            this.lbLastName.Name = "lbLastName";
+            this.lbLastName.Size = new System.Drawing.Size(58, 13);
+            this.lbLastName.TabIndex = 2;
+            this.lbLastName.Text = "Last Name";
+            // 
+            // tbHiring
+            // 
+            this.tbHiring.Location = new System.Drawing.Point(101, 90);
+            this.tbHiring.Name = "tbHiring";
+            this.tbHiring.Size = new System.Drawing.Size(126, 20);
+            this.tbHiring.TabIndex = 5;
+            // 
+            // lbHiringDate
+            // 
+            this.lbHiringDate.AutoSize = true;
+            this.lbHiringDate.Location = new System.Drawing.Point(11, 93);
+            this.lbHiringDate.Name = "lbHiringDate";
+            this.lbHiringDate.Size = new System.Drawing.Size(72, 13);
+            this.lbHiringDate.TabIndex = 4;
+            this.lbHiringDate.Text = "Date of Hiring";
+            // 
+            // tbRelease
+            // 
+            this.tbRelease.Location = new System.Drawing.Point(101, 116);
+            this.tbRelease.Name = "tbRelease";
+            this.tbRelease.Size = new System.Drawing.Size(126, 20);
+            this.tbRelease.TabIndex = 7;
+            // 
+            // lbEmploymentDate
+            // 
+            this.lbEmploymentDate.AutoSize = true;
+            this.lbEmploymentDate.Location = new System.Drawing.Point(11, 119);
+            this.lbEmploymentDate.Name = "lbEmploymentDate";
+            this.lbEmploymentDate.Size = new System.Drawing.Size(84, 13);
+            this.lbEmploymentDate.TabIndex = 6;
+            this.lbEmploymentDate.Text = "Date of Release";
+            // 
+            // tbComments
+            // 
+            this.tbComments.Location = new System.Drawing.Point(101, 142);
+            this.tbComments.Name = "tbComments";
+            this.tbComments.Size = new System.Drawing.Size(126, 20);
+            this.tbComments.TabIndex = 11;
+            // 
+            // lbComments
+            // 
+            this.lbComments.AutoSize = true;
+            this.lbComments.Location = new System.Drawing.Point(12, 145);
+            this.lbComments.Name = "lbComments";
+            this.lbComments.Size = new System.Drawing.Size(56, 13);
+            this.lbComments.TabIndex = 10;
+            this.lbComments.Text = "Comments";
+            // 
+            // tbSalary
+            // 
+            this.tbSalary.Location = new System.Drawing.Point(101, 168);
+            this.tbSalary.Name = "tbSalary";
+            this.tbSalary.Size = new System.Drawing.Size(126, 20);
+            this.tbSalary.TabIndex = 13;
+            // 
+            // lbSalary
+            // 
+            this.lbSalary.AutoSize = true;
+            this.lbSalary.Location = new System.Drawing.Point(12, 171);
+            this.lbSalary.Name = "lbSalary";
+            this.lbSalary.Size = new System.Drawing.Size(36, 13);
+            this.lbSalary.TabIndex = 12;
+            this.lbSalary.Text = "Salary";
+            // 
+            // Cancel
+            // 
+            this.Cancel.Location = new System.Drawing.Point(146, 194);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(81, 23);
+            this.Cancel.TabIndex = 14;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(12, 194);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(80, 23);
+            this.btnConfirm.TabIndex = 15;
+            this.btnConfirm.Text = "Confirm";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            // 
+            // btnDismiss
+            // 
+            this.btnDismiss.Location = new System.Drawing.Point(146, 223);
+            this.btnDismiss.Name = "btnDismiss";
+            this.btnDismiss.Size = new System.Drawing.Size(80, 23);
+            this.btnDismiss.TabIndex = 16;
+            this.btnDismiss.Text = "Dismiss";
+            this.btnDismiss.UseVisualStyleBackColor = true;
+            // 
+            // tbEmployeeId
+            // 
+            this.tbEmployeeId.Enabled = false;
+            this.tbEmployeeId.Location = new System.Drawing.Point(101, 12);
+            this.tbEmployeeId.Name = "tbEmployeeId";
+            this.tbEmployeeId.Size = new System.Drawing.Size(126, 20);
+            this.tbEmployeeId.TabIndex = 18;
+            // 
+            // lbId
+            // 
+            this.lbId.AutoSize = true;
+            this.lbId.Location = new System.Drawing.Point(11, 15);
+            this.lbId.Name = "lbId";
+            this.lbId.Size = new System.Drawing.Size(65, 13);
+            this.lbId.TabIndex = 17;
+            this.lbId.Text = "Employee Id";
+            // 
+            // AddUpdateEmployee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 252);
+            this.Controls.Add(this.tbEmployeeId);
+            this.Controls.Add(this.lbId);
+            this.Controls.Add(this.btnDismiss);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.tbSalary);
+            this.Controls.Add(this.lbSalary);
+            this.Controls.Add(this.tbComments);
+            this.Controls.Add(this.lbComments);
+            this.Controls.Add(this.tbRelease);
+            this.Controls.Add(this.lbEmploymentDate);
+            this.Controls.Add(this.tbHiring);
+            this.Controls.Add(this.lbHiringDate);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.lbLastName);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.lbFirstName);
+            this.Name = "AddUpdateEmployee";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Add New Employee";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbFirstName;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.Label lbLastName;
+        private System.Windows.Forms.TextBox tbHiring;
+        private System.Windows.Forms.Label lbHiringDate;
+        private System.Windows.Forms.TextBox tbRelease;
+        private System.Windows.Forms.Label lbEmploymentDate;
+        private System.Windows.Forms.TextBox tbComments;
+        private System.Windows.Forms.Label lbComments;
+        private System.Windows.Forms.TextBox tbSalary;
+        private System.Windows.Forms.Label lbSalary;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Button btnDismiss;
+        private System.Windows.Forms.TextBox tbEmployeeId;
+        private System.Windows.Forms.Label lbId;
+    }
+}
