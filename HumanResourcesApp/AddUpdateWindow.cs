985728d7d1d9c39ff27976426c237847913cd942
RokerDev <rokerdev91@gmail.com>
2022-07-12 20:03:14 +0200

    add HireNewEmployee method

--- HumanResourcesApp/AddUpdateWindow.cs
+++ HumanResourcesApp/AddUpdateWindow.cs
@@ -13,9 +13,24 @@ namespace HumanResourcesApp
     public partial class AddUpdateEmployee : Form
     {
         private FileHelper<List<Employee>> _fileHelper = new FileHelper<List<Employee>>(Program.FilePath);
+
         public AddUpdateEmployee()
         {
             InitializeComponent();
+        }
+
+        private Employee HireNewEmpolyee()
+        {
+            return new Employee
+            {
+                Id = SetNewId(),
+                FirstName = tbFirstName.Text,
+                LastName = tbLastName.Text,
+                HiringDate = Convert.ToDateTime(tbHiring.Text),
+                ReleaseDate = Convert.ToDateTime(tbRelease.Text),
+                Comments = tbComments.Text,
+                Salary = Convert.ToInt32(tbSalary.Text)
+            };
         }
     }
 }
